4482bfeacc7cc49bb5f306beae855897a7f5856e
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-04-11 07:50:53 +0200

    bind TextEditor.text with content of textbox

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-import tkinter 
-import customtkinter 
+import tkinter as tk
+import customtkinter
+from TextEditor import TextEditor
 
 customtkinter.set_default_color_theme("dark-blue")
 customtkinter.set_appearance_mode("System")
+
 
 class TextEditorGui(customtkinter.CTk):
     def __init__(self):
 
         super().__init__()
 
-        # self.root = customtkinter.CTk()
+        self.editor = TextEditor()
         self.title("Text Editor")
         self.minsize(400, 300)
         self.textbox = customtkinter.CTkTextbox(master=self)
         self.textbox.pack()
 
+        self.textbox.bind("<Key>", self.update_text)
+
+        self.undo_button = customtkinter.CTkButton(master=self, text="Undo")
+        self.undo_button.pack(side=tk.LEFT)
+
+    def undo(self):
+        self.editor.undo()
+        self.update_textbox()
+
+    def update_textbox(self):
+        self.textbox.delete("1.0", tk.END)
+        self.textbox.insert(tk.END, self.editor.text)
+
+    def update_text(self, event):
+        self.editor.text = self.textbox.get("1.0", tk.END)
+        print(self.editor.text)
 
 
 if __name__ == "__main__":
