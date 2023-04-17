fa465cfa49cd9401147c42769218b9f418edd018
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-04-17 11:31:59 +0200

    remove text editor file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 import customtkinter
-from TextEditor import TextEditor
 
 customtkinter.set_default_color_theme("dark-blue")
 customtkinter.set_appearance_mode("System")
@@ -11,47 +10,40 @@ class TextEditorGui(customtkinter.CTk):
 
         super().__init__()
 
-        self.editor = TextEditor()
+        self.text = ""
         self.title("Text Editor")
-        self.minsize(400, 300)
+        self.minsize(800, 600)
         self.textbox = customtkinter.CTkTextbox(master=self)
-        self.textbox.pack()
+        self.textbox.pack(expand=1, fill="both")
+        self.textbox.bind("<KeyRelease>", self.update_text)
 
-        self.textbox.bind("<Key>", self.update_text)
-
-        self.load_button = customtkinter.CTkButton(master=self, text="Load", command=self.load)
+        self.load_button = customtkinter.CTkButton(
+            master=self, text="Load", command=self.load)
         self.load_button.pack(side=tk.LEFT)
 
-        # self.redo_button = customtkinter.CTkButton(master=self, text="Redo")
-        # self.redo_button.pack(side=tk.RIGHT)
-
-
-        self.save_button = customtkinter.CTkButton(master=self, text="Save", command=self.save)
+        self.save_button = customtkinter.CTkButton(
+            master=self, text="Save", command=self.save)
         self.save_button.pack(side=tk.RIGHT)
-
 
     def save(self):
         filename = tk.filedialog.asksaveasfilename(defaultextension=".txt")
         with open(filename, "w") as f:
-            f.write(self.editor.text)
+            f.write(self.text)
 
     def load(self):
         filename = tk.filedialog.askopenfilename(defaultextension=".txt")
         with open(filename, "r") as f:
-            self.editor.text = f.read()
+            self.text = f.read()
         self.update_textbox()
 
-    def undo(self):
-        self.editor.undo()
-        self.update_textbox()
 
     def update_textbox(self):
         self.textbox.delete("1.0", tk.END)
-        self.textbox.insert(tk.END, self.editor.text)
+        self.textbox.insert(tk.END, self.text)
 
     def update_text(self, event):
-        self.editor.text = self.textbox.get("1.0", tk.END)
-        print(self.editor.text)
+        self.text = self.textbox.get(1.0, 'end-1c')
+        print(self.text)
 
 
 if __name__ == "__main__":
